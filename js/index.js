a13d2347d287dc38855b34f65da72507b105ac45
Paul-Semenko <78956330+Paul-Semenko@users.noreply.github.com>
2021-08-19 13:47:00 +0300

    commit-11-02

--- js/index.js
+++ js/index.js
@@ -5,20 +5,25 @@ console.log(buttonStop);
 
 buttonStart.addEventListener('click', colorChangeStart);
 
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548', ];
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
 
 function switcher() {
-    const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548', ];
-    const bodyBgChange = Math.floor(Math.random() * colors.length);
-    const selectedColor = colors[bodyBgChange];
+    const randomColorIndex = randomIntegerFromInterval(0, colors.length - 1);
+    const selectedColor = colors[randomColorIndex];
     document.body.style.backgroundColor = selectedColor;
-    buttonStart.setAttribute('disabled', true);
-
 }
 
 let id = null;
 
 function colorChangeStart() {
     id = setInterval(switcher, 1000);
+    buttonStart.setAttribute('disabled', true);
 }
 
 function colorChangeStop() {
